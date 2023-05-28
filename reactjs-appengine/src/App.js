7cfbb15866b6dcc76cc2499d1589ed58b4b7a5c7
Manohar S <manohar427@gmail.com>
2023-05-28 13:56:34 +0530

    GCP App engine & React JS demo

---
+++ reactjs-appengine/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Zest Prime GCP App engine React JS demo.
+      </header>
+    </div>
+  );
+}
+
+export default App;
